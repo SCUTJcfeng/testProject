0fbe36b9d6e28a98b0c76860c1af561022ac262a
fengjc <jcfeng2013@gmail.com>
2019-05-31 13:22:44 +0800

    add pytest skip

--- test_math_pytest.py
+++ test_math_pytest.py
@@ -4,9 +4,10 @@
 Project: testProject
 Author: jc feng (jcfeng2013@gmail.com)
 File Created: 2019-05-31 12:40:07
-Last Modified: 2019-05-31 12:54:06
+Last Modified: 2019-05-31 13:20:58
 '''
 
+import pytest
 from main import add, minus, multi
 
 
@@ -19,4 +20,12 @@ class TestMathPytest:
 
     def test_multi(self):
         assert 0 == multi(0, 2)
+        assert 2 == multi(1, 2)
+
+    @pytest.mark.skip('skip multi')
+    def test_multi_2(self):
+        assert 2 == multi(1, 2)
+
+    def test_multi_3(self):
+        pytest.skip('skip multi')
         assert 2 == multi(1, 2)
